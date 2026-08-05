aa871afd62567581976713c6a9f275a739297460
Mateus Burlamaqui Moreira <mateusblm@outlook.com>
2026-08-05 20:04:25 -0300

    test: configurando perfil de testes com banco H2

--- src/test/java/com/velsis/java/teste/mateusburlamaqui/usuarios_api/UsuariosApiApplicationTests.java
+++ src/test/java/com/velsis/java/teste/mateusburlamaqui/usuarios_api/UsuariosApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.velsis.java.teste.mateusburlamaqui.usuarios_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UsuariosApiApplicationTests {
 
 	@Test
